202d8def1fbf3fd1dcc07044298ae661fc8f40a5
LittleElijah <cuiyeqing960904@gmail.com>
2018-09-10 17:50:51 +0300

    vuex in nuxt set up

---
+++ cartclient/layouts/partials/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar is-white">
+      <div class="container">
+      <div class="navbar-brand">
+        <nuxt-link :to="{ name: 'index' }" class="navbar-item">
+          cart
+      </nuxt-link>
+        <div class="navbar-burger burger" data-target="nav">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="nav" class="navbar-menu">
+        <div class="navbar-end">
+          <a href="" class="navbar-item">
+            Sign in
+          </a>
+        </div>
+      </div>
+      </div>
+    </nav>
+</template>
