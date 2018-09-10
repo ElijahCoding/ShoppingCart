c7bcee516ab5dacca3bbe0936e8ce1e5d6b6c5be
LittleElijah <cuiyeqing960904@gmail.com>
2018-09-10 17:21:12 +0300

    Category index endpoint test

--- tests/Feature/Categories/CategoryIndexTest.php
+++ tests/Feature/Categories/CategoryIndexTest.php
@@ -17,7 +17,37 @@ class CategoryIndexTest extends TestCase
              ->assertJsonFragment([
                 'slug' => $categories->get(0)->slug
             ],[
-                'slug' => $categories->get(1)->slug 
+                'slug' => $categories->get(1)->slug
             ]);
     }
+
+    public function test_it_returns_only_parent_categories()
+    {
+        $category = factory(Category::class)->create();
+
+        $category->children()->save(
+            factory(Category::class)->create()
+        );
+
+        $this->json('GET', 'api/categories')
+             ->assertJsonCount(1, 'data');
+    }
+
+    public function test_it_returns_categories_ordered_by_their_given_order()
+    {
+        $category = factory(Category::class)->create([
+            'order' => 2
+        ]);
+
+        $anotherCategory = factory(Category::class)->create([
+            'order' => 1
+        ]);
+
+        $this->json('GET', 'api/categories')
+             ->assertSeeInOrder([
+                 $anotherCategory->slug,
+                 $category->slug
+             ]);
+    }
+
 }
