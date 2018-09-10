ef2d890de39023d1aa8ddb2d1e9a60789adbb151
LittleElijah <cuiyeqing960904@gmail.com>
2018-09-10 20:06:29 +0300

    product set up

---
+++ tests/Unit/Products/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Products;
+
+use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductTest extends TestCase
+{
+    public function test_it_uses_the_slug_for_the_route_key_name()
+    {
+        $product = new Product();
+
+        $this->assertEquals($product->getRouteKeyName(), 'slug');
+    }
+}
