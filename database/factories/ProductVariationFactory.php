f1c066f0fb37eff1ece597dc379947c3c32dacc1
LittleElijah <cuiyeqing960904@gmail.com>
2018-09-11 21:23:36 +0300

    product variation unit test set up

---
+++ database/factories/ProductVariationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+use App\Models\ProductVariation;
+
+$factory->define(ProductVariation::class, function (Faker $faker) {
+    return [
+        'product_id' => factory(Product::class)->create()->id,
+        'name' => $faker->unique()->name,
+    ];
+});
--- tests/Unit/Products/ProductTest.php
+++ tests/Unit/Products/ProductTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Products;
 use Tests\TestCase;
 use App\Models\Product;
 use App\Models\Category;
+use App\Models\ProductVariation;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -26,5 +27,16 @@ class ProductTest extends TestCase
         );
 
         $this->assertInstanceOf(Category::class, $product->categories->first());
+    }
+
+    public function test_it_has_many_variations()
+    {
+        $product = factory(Product::class)->create();
+
+        $product->variations()->save(
+            factory(ProductVariation::class)->create()
+        );
+
+        $this->assertInstanceOf(ProductVariation::class, $product->variations->first());
     }
 }
